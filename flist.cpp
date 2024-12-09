c1a1c8d63e3f59f422263a22ec84bb875a9fc760
root <root@HOME-PC>
2024-12-10 02:49:26 +0700

    flist done

--- flist.cpp
+++ flist.cpp
@@ -1,87 +1,123 @@
 #include <iostream>
 using namespace std;
 
-struct Node{
-    int data;
+template <typename T>
+struct Node {
+    T val;
     Node* next;
-    Node(int value) : data(value), next(nullptr) {}
+
+    Node(T _val) : val(_val), next(nullptr) {}
 };
 
-class flist{
-    private:
-    Node *head;
+template <typename T>
+struct list {
+    Node<T>* head;
+    Node<T>* tail;
 
-    public:
-    flist(): head(nullptr){}
+    list() : head(nullptr), tail(nullptr) {}
 
-    ~flist(){
-        while (head != nullptr) {
-            Node* temp = head;
-            head = head->next;
-            delete temp;
+    bool is_empty() {
+        return head == nullptr;
+    }
+
+    void push_back(T _val) {
+        Node<T>* p = new Node(_val);
+        if (is_empty()) {
+            head = p;
+            tail = p;
+            return;
         }
+        tail->next = p;
+        tail = p;
     }
 
-    //функции
-    void print(){ //O(n)
-        Node* current = head;
-        while (current != nullptr){
-            cout << current->data << " -> ";
-            current = current->next;
+    void print() {
+        if (is_empty()) return;
+        Node<T>* p = head;
+        while (p) {
+            cout << p->val << " ";
+            p = p->next;
         }
-        cout << "nullptr" << endl;
-    }
-    
-    void add_konec(int value){ //O(n)
-        Node* newNode = new Node(value);
-        if (head == nullptr) { // Если список пуст, новый узел становится головой
-            head = newNode;
-        } else {
-            Node* current = head;
-            while (current->next != nullptr) { // Находим последний узел
-                current = current->next;
-            }
-            current->next = newNode; // Присоединяем новый узел
-        }
+        cout << endl;
     }
 
-    void add_nachalo(int value){ //O(1)
-        Node* newNode = new Node(value);
-        newNode->next = head;
-        head = newNode;
-    }
-    
-    void del_nachalo(){ //O(1)
-        Node* delNode = head;
-        head = delNode->next;
-        delete delNode;
+    Node<T>* find(T _val) {
+        Node<T>* p = head;
+        while (p && p->val != _val) p = p->next;
+        return (p && p->val == _val) ? p : nullptr;
     }
 
-    void del_konec(){ //O(n)
-        Node* delNode = head;
-        while (delNode->next->next != nullptr) { // Находим предпоследний узел
-            delNode = delNode->next;
-        }
-        Node* predposl = delNode;
-        predposl->next = nullptr;
-        delNode = delNode->next;
-        delete delNode;
+    void remove_first() {
+        if (is_empty()) return;
+        Node<T>* p = head;
+        head = p->next;
+        delete p;
     }
-    
+
+    void remove_last() {
+        if (is_empty()) return;
+        if (head == tail) {
+            remove_first();
+            return;
+        }
+        Node<T>* p = head;
+        while (p->next != tail) p = p->next;
+        p->next = nullptr;
+        delete tail;
+        tail = p;
+    }
+
+    void remove(T _val) {
+        if (is_empty()) return;
+        if (head->val == _val) {
+            remove_first();
+            return;
+        }
+        else if (tail->val == _val) {
+            remove_last();
+            return;
+        }
+        Node<T>* slow = head;
+        Node<T>* fast = head->next;
+        while (fast && fast->val != _val) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        if (!fast) {
+            cout << "This element does not exist" << endl;
+            return;
+        }
+        slow->next = fast->next;
+        delete fast;
+    }
+
+    Node<T>* operator[] (const int index) {
+        if (is_empty()) return nullptr;
+        Node<T>* p = head;
+        for (int i = 0; i < index; i++) {
+            p = p->next;
+            if (!p) return nullptr;
+        }
+        return p;
+    }
 };
 
-
-
-int main(){
-    flist list;
-    list.add_konec(1);
-    list.add_konec(2);
-    list.print();
-    list.add_nachalo(0);
-    list.add_nachalo(-1);
-    list.print();
-    list.del_nachalo();
-    list.del_konec();
-    list.print();
+int main()
+{
+    list <string> l;
+    cout << l.is_empty() << endl;
+    l.push_back("3");
+    l.push_back("123");
+    l.push_back("8");
+    l.print();
+    cout << l.is_empty() << endl;
+    l.remove("123");
+    l.print();
+    l.push_back("1234");
+    l.remove_first();
+    l.print();
+    l.remove_last();
+    l.print();
+    cout << l[0]->val << endl;
     return 0;
 }
